8974a9f539cc7dcd1e4611a52ee027ea538607bb
nancytao <nancy.tao42@gmail.com>
2016-07-21 12:45:43 +0200

    Registration that kind of works

--- GTTravel.py
+++ GTTravel.py
@@ -1,12 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    return render_template('Register.html')
 
+@app.route('/Register.html/', methods=["GET","POST"])
+def register():
+    try:
+        form = RegistrationForm(request.form)
+
+        if request.method == "POST" and form.validate():
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+            c, conn = connection()
+
+            x = c.execute("SELECT * FROM USERS WHERE username = (%s)", (thwart(username)))
+            y = c.execute("SELECT * FROM USERS WHERE email = (%s)", (thwart(email)))
+
+            if int(x) > 0:
+                flash("Username taken")
+                return render_template('Register.html', form=form)
+            elif int(y) > 0:
+                flash("Email previously used")
+                return render_template('Register.html', form=form)
+            else:
+                c.execute("INSERT INTO users (username, password, email, tracking) VALUES (%s, %s, %s, %s)",
+                         (thwart(username), thwart(password), thwart(email), thwart("/introduction-to-python-programming/")))
+
+                conn.commit()
+                flash("Thanks for registering!")
+                c.close()
+                conn.close()
+                gc.collect()
+
+                session['logged_in'] = True
+                session['username'] = username
+
+                return redirect(url_for('dashboard'))
+        return render_template("Register.html", form=form)
+
+    except Exception as e:
+        return(str(e))
 
 if __name__ == '__main__':
     app.run()
