2f52ddab67db86f018ef59e4cbe6e730e1d75381
nancytao <nancy.tao42@gmail.com>
2016-07-22 18:14:52 +0200

    Connecting to database from python script

---
+++ test.py
@@ -0,0 +1,14 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost",
+                     user="root",
+                     passwd="",
+                     db="gttravel")
+
+cur = db.cursor()
+cur.execute("SELECT * FROM USERS")
+
+for row in cur.fetchall():
+    print row
+
+db.close()
