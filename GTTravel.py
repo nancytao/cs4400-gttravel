8d3b5cff8dbac7fada1d8e73784d181952244396
Cole Bowers <cole.a.bowers@gmail.com>
2016-07-23 13:26:22 +0200

    Connecting webpages through flask

--- GTTravel.py
+++ GTTravel.py
@@ -13,5 +13,39 @@ def test():
     return render_template('test.html')
 
 
+@app.route("/to_home")
+def to_home():
+    return render_template('homepage.html')
+
+
+@app.route("/to_country_search")
+def to_country_search():
+    return render_template('countrysearch.html')
+
+
+@app.route("/to_city_search")
+def to_city_search():
+    return render_template('citysearch.html')
+
+
+@app.route("/to_location_search")
+def to_location_search():
+    return render_template('locationsearch.html')
+
+
+@app.route("/to_event_search")
+def to_event_search():
+    return render_template('eventsearch.html')
+
+
+@app.route("/to_write_reviews")
+def to_write_reviews():
+    return render_template('writereviews.html')
+
+
+@app.route("/to_past_reviews")
+def to_past_reviews():
+    return render_template('pastreviews.html')
+
 if __name__ == '__main__':
     app.run()
