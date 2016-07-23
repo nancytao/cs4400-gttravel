218b3b3f67ef6ce43ed9ad4ef3f93ee7267558bb
nancytao <nancy.tao42@gmail.com>
2016-07-23 14:43:55 +0200

    Break lines on strings

--- test.py
+++ test.py
@@ -8,12 +8,15 @@ db = MySQLdb.connect(host="localhost",
 
 cur = db.cursor()
 
-cur.execute("INSERT INTO users(Username, Email, Password, Is_manager) VALUES ('name','filler@gmail.com','password',FALSE)")
+cur.execute("INSERT INTO users(Username, Email, Password, Is_manager) VALUES"\
+    "('name','filler@gmail.com','password',FALSE)")
 
 cur.execute("SELECT * FROM USERS")
 for row in cur.fetchall():
     print row[0]
 
-db.commit()
+# db.commit()
 
 db.close()
+
+
