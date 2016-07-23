9ccdd75dd653625c515e733d7da927e8053b5188
Cole Bowers <cole.a.bowers@gmail.com>
2016-07-23 13:54:52 +0200

    Connecting webpages through flask All connections complete

--- GTTravel.py
+++ GTTravel.py
@@ -5,12 +5,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('homepage.html')
+    return render_template('login.html')
 
 
 @app.route("/testing")
 def test():
     return render_template('test.html')
+
+
+@app.route("/to_register")
+def to_register():
+    return render_template('register.html')
+
+
+@app.route("/register")
+def register():
+    return render_template('homepage.html')
+
+
+@app.route("/to_login")
+def to_login():
+    return render_template('login.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('homepage.html')
 
 
 @app.route("/to_home")
